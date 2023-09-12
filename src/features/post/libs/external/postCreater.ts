16129a11debf79af85cab50559829a6b5e339c29
TanakaKazushiro <tnk30125146@gmail.com>
2023-09-12 23:01:43 +0900

    認証機能追加

--- src/features/post/libs/external/postCreater.ts
+++ src/features/post/libs/external/postCreater.ts
@@ -1,26 +1,31 @@
-import axios from 'axios';
-import {login} from '@/features/user/libs/external/login'
+import axios from "axios";
+import { login } from "@/features/user/libs/external/login";
 
+export const postCreater = async (
+  title: string,
+  content: string,
+  category_id: string
+) => {
+  const body = {
+    title,
+    content,
+  } as { title: string; content: string; category?: string };
+  if (category_id !== "") {
+    body["category"] = category_id;
+  }
 
-export const postCreater = async (title:string,content:string,category_id:string) => {
-
-    const body = {
-        title,
-        content,
-    } as {title:string,content:string,category?:string}
-    if(category_id !== ""){
-        body["category"] = category_id;
-    }
-
-    try {
-    const token = await login("tanakakazushiro","testadmin");
+  try {
+    const token = await login("tanakakazushiro", "testadmin");
     const config = {
-        headers: { Authorization: `Bearer ${token}` }
-      };
-    const response = await axios.post("http://localhost:8000/blog/posts/", body,config);
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const response = await axios.post(
+      "http://localhost:8000/blog/posts/",
+      body,
+      config
+    );
     return response.data;
-    } catch (error) {
+  } catch (error) {
     console.log("error", error);
-    }
-
-}
+  }
+};
--- src/features/sideber/ManagePanel.tsx
+++ src/features/sideber/ManagePanel.tsx
@@ -1,40 +1,40 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
-import { Card } from '../base/components/container/Card'
-import { Block } from '../base/components/container/Block'
-import { Title } from '../base/components/display/Title'
-import {BsKeyFill} from 'react-icons/bs'
-import { useAuthContext } from '../user/contexts/AuthContext'
+import { Card } from "../base/components/container/Card";
+import { Block } from "../base/components/container/Block";
+import { Title } from "../base/components/display/Title";
+import { BsKeyFill } from "react-icons/bs";
+import { useAuthContext } from "../user/contexts/AuthContext";
 
 export const ManagePanel = () => {
-
-  const {session} = useAuthContext();
+  const { session } = useAuthContext();
 
   return (
-    <Card className='w-full p-4'>
-        <Title className='max-w-fit border-b-2 border-gray-400' >管理者</Title>
+    <Card className="w-full p-4">
+      <Title className="max-w-fit border-b-2 border-gray-400">管理者</Title>
 
-        {session?(
-        <Link href="/manage" className='corsor-pointer'>
-            <Block
-                className={`
+      {session ? (
+        <Link href="/manage" className="corsor-pointer">
+          <Block
+            className={`
                 mt-2 p-1
                 flex items-center flex-row space-x-1
                 border-transparent
                 text-gray-500 border-b hover:border-gray-400`}
-            >
-
-                <BsKeyFill className='w-6 h-6'/>
-                <Block>記事管理</Block>
-            </Block>    
-        </Link>):(
-          <Link href="/login"
-                className="block bg-gray-500 text-white p-2 rounded-lg mt-1"
-          >ログイン
-          </Link>
-          
-        )}
+          >
+            <BsKeyFill className="w-6 h-6" />
+            <Block>記事管理</Block>
+          </Block>
+        </Link>
+      ) : (
+        <Link
+          href="/login"
+          className="block bg-gray-500 text-white p-2 rounded-lg mt-1"
+        >
+          ログイン
+        </Link>
+      )}
     </Card>
-  )
-}
+  );
+};
